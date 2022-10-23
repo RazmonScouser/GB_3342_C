db31626b47e0196f3e0b21c2a5a421f9211aa598
Razmik <razmique@gmail.com>
2022-10-23 19:00:41 +0300

    Added task02

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -2,11 +2,19 @@
 Console.WriteLine("Enter two numbers:");
 int number1 = Convert.ToInt32(Console.ReadLine());
 int number2 = Convert.ToInt32(Console.ReadLine());
-int min = number1; 
+int min = number1;
+int max = number2; 
 if (number1 < number2) {
 min = number1;
 }
 else{
 min = number2;
 }
+if (number1 > number2) {
+max = number1;
+}
+else{
+max = number2;
+}
 Console.WriteLine($"The minimum is {min}");
+Console.WriteLine($"The maximum is {max}");
