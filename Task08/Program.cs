a774cc4a53f6e4c6d9e9c4fc8c45a8f55c028bca
Razmik <razmique@gmail.com>
2022-10-24 00:07:41 +0300

    Changed Task08

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 Console.WriteLine("Enter a limit number");
 int number = Convert.ToInt32(Console.ReadLine());
 int count = 1;
