5a08c5bc3211f954ed08526c42aee0b01a8ce7e6
Razmik <razmique@gmail.com>
2022-10-23 22:54:02 +0300

    Added task08

---
+++ Task08/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter a limit number");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+int even_factor=0;
+while (count <= number)
+{
+    even_factor=count%2;
+    if (even_factor==0)
+    {
+        Console.Write($"{count} ");
+    }
+    count++;
+}
