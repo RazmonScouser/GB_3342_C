3cbf3bd3e4f290d61bc9b561e67da559f124401c
Razmik <razmique@gmail.com>
2022-10-23 22:57:52 +0300

    Changed task04

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -4,10 +4,12 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 int number2 = Convert.ToInt32(Console.ReadLine());
 int number3 = Convert.ToInt32(Console.ReadLine());
 int max = number1;
-if (number2 > max ) {
+if (number2 > max ) 
+{
 max = number2;
 }
-if(number3>max) {
+if(number3>max) 
+{
     max = number3;
 }
 Console.WriteLine($"The maximum is {max}");
