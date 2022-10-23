130dc512b6f39881e70b2fb867df094049e776ee
Razmik <razmique@gmail.com>
2022-10-23 23:26:16 +0300

    Added Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,6 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. 
+//Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+Console.WriteLine("Enter a three digit number");
+int number = Convert.ToInt32(Console.ReadLine());
+int second_digit = number % 100 / 10;
+Console.WriteLine(second_digit);
