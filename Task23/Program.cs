e7e863ff454c00be1086865bd1408d867fb74cd5
Razmik <razmique@gmail.com>
2022-11-10 01:19:04 +0300

    Added Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+//5 ->
+//1 | 1
+//2 | 8
+//3 | 27
+//4 | 64
+//5 | 125
+Console.WriteLine("Enter a number");
+int number1 = Convert.ToInt32(Console.ReadLine());
+void Cube(int number)
+{
+    if (number >= 1)
+    {
+        for (int i = 1;i <= number; i++)
+        {
+            Console.WriteLine($"{i} | {i*i*i}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Enter a number bigger than 1");
+    }
+}
+Cube(number1);
