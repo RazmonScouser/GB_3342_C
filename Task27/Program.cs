168c5a765b82ab965b599919b3ac6bb7f79ae4ad
Razmik <razmique@gmail.com>
2022-11-21 10:48:51 +0300

    Added Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,21 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+//452 -> 11
+
+//82 -> 10
+
+//9012 -> 12
+
+Console.WriteLine("Enter a number");
+int number1= Convert.ToInt32(Console.ReadLine());
+void SumCyph(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum = sum + (number%10);
+        number = number/10;
+    }
+    Console.WriteLine(sum);
+}
+SumCyph(number1);
