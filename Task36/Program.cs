e516911f26d777c798ec1f6a02f3651cee490210
Razmik <razmique@gmail.com>
2022-11-21 12:29:25 +0300

    Added Task36

---
+++ Task36/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+int [] array =  new int [10];
+int sumposuneven = 0;
+Console.Write("[");
+for (int i = 0; i < array.Length;i++ )
+{
+    array[i] = new Random().Next(-100,100);
+    Console.Write(array[i]);
+    if (i != array.Length-1)
+    {
+        Console.Write(", ");
+    }
+    if (i % 2 ==0)
+    {
+        sumposuneven = sumposuneven + array[i];
+    }
+}
+Console.WriteLine("]");
+Console.WriteLine(sumposuneven);
